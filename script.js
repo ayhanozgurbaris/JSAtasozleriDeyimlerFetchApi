0ee50b875aad64d27e00b4d6d767d30579ead332
ayhanozgurbaris <barisayhan01@gmail.com>
2022-06-14 16:50:35 +0300

    push

---
+++ script.js
@@ -0,0 +1,56 @@
+const result = document.getElementById("result");
+const searchBox = document.getElementById("searchBox");
+const searchList = document.getElementById("searchList");
+
+
+const keywords = [];
+const proverbs = [];
+
+loadResults();
+
+async function loadResults(){
+    const serverResponse=await fetch('https://sozluk.gov.tr/atasozu');
+    let datas=await serverResponse.json();
+    console.log(datas);
+
+    datas.forEach(element => {
+        keywords.push(element.key);
+        proverbs.push(element.sozum);
+    });
+    
+    const birlesmisKelimeler = [...new Set(keywords)];
+    console.log(birlesmisKelimeler);
+
+    birlesmisKelimeler.sort(() => Math.random() -0.5)
+    let counter = 0;
+    birlesmisKelimeler.forEach(element => {
+        if (counter<5) {
+            const newSuggestion = document.createElement("option");
+            searchList.appendChild(newSuggestion);
+            newSuggestion.value = element;
+        }
+
+        counter++;
+    });
+
+}
+
+
+searchBox.addEventListener("input",(e) => resultlarFiltrele(e.target.value));
+
+function resultlarFiltrele(searchedWord){
+    result.innerHTML="";
+    const searchCriteria = new RegExp(searchedWord,"gi");
+    let matched = proverbs.filter(soz => searchCriteria.test(soz));
+
+    if (searchedWord.length === 0 ) {
+        matched = [];
+    }
+
+    //console.log(matched);
+    matched.forEach(es =>{
+        const nextresult = document.createElement("li");
+        result.appendChild(nextresult);
+        nextresult.innerHTML = es;
+    })
+}
